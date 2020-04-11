6fe9add1a69796126e5a2c49f4a62e3dbb268d70
Kyle Tran <kylevtran201@gmail.com>
2020-04-10 23:36:57 -0700

    Finished is_permutation

---
+++ CH1/is_permutation_kyle.py
@@ -0,0 +1,38 @@
+""" Given two strings, write a method to decide if one is a permutation of the other. """
+import collections, unittest
+
+def is_permutation(s1, s2):
+    n1, n2 = len(s1), len(s2)
+    if n1 != n2:
+        return False
+
+    chars = collections.defaultdict(int)
+    for i in range(n1):
+        chars[s1[i]] += 1
+        chars[s2[i]] -= 1
+
+    for count in chars.values():
+        if count != 0:
+            return False
+
+    return True
+
+
+class TestStrings(unittest.TestCase):
+    def test_isUnique(self):
+        s1 = "cat"
+        s2 = "tac"
+        s3 = "catyy" 
+        s4, s5 = "", ""
+        s6, s7 = "rats", "star"
+        s8, s9 = "doggy", "gogdy"
+        s10, s11 = "kitten", "cat"
+        self.assertTrue(is_permutation(s1, s2))
+        self.assertFalse(is_permutation(s1, s3))
+        self.assertTrue(is_permutation(s4,s5))
+        self.assertTrue(is_permutation(s6, s7))
+        self.assertTrue(is_permutation(s8, s9))
+        self.assertFalse(is_permutation(s10, s11))
+
+
+unittest.main()
