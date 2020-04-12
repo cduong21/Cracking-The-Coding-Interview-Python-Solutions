8ad5b237d858c7f57efff9f055ff6c79fd90abe8
Kyle Tran <kylevtran201@gmail.com>
2020-04-11 23:51:06 -0700

    Working version palindrome_permuatation

---
+++ CH1/palindrome_permutation_kyle.py
@@ -0,0 +1,33 @@
+import collections
+test = "Tact Coa"
+
+def palindrome_permutation(string):
+    string = string.lower().replace(" ", "")
+    n = len(string)
+
+    print(string)
+    print(n)
+
+    char_count = collections.defaultdict(int)
+    for c in string:
+        char_count[c] += 1
+
+    print(char_count)
+    #If our string is even
+    if n % 2 == 0:
+        for char in char_count:
+            if char_count[char] % 2 != 0:
+                return False
+    # If our string is odd
+    else:
+        odd_flag = False
+        for char in char_count:
+            if char_count[char] % 2 != 0:
+                if odd_flag == True:
+                    return False
+                else:
+                    odd_flag = True
+
+    return(True)
+
+print(palindrome_permutation(test))
