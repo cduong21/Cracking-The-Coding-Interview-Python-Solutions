9b5a8b8135294c3965715521970776c953316ffc
cduong21 <cduong21@cmc.edu>
2020-04-12 04:03:33 -0700

    lol

--- CH1/one_away_cindy.py
+++ CH1/one_away_cindy.py
@@ -18,7 +18,6 @@ def one_away(s1, s2):
     n2 = len(s2)
     tracker = collections.defaultdict(int)
     longer = max(n1, n2)
-    print(longer)
 
     if abs(n1-n2) > 1:
         return False 
@@ -27,20 +26,23 @@ def one_away(s1, s2):
         if abs(n1-n2) == 1:
             tracker[s1[i]] += 1 
             tracker[s2[i]] -= 1
-            if len(tracker) == 1:
-                return True 
-            else: 
-                return False 
+            if tracker[i] == 0:
+                del tracker[i]
+        if len(tracker) == 1:
+            return True 
+        else: 
+            return False 
         
         if n1 == n2:
             tracker[s1[i]] += 1
-            print(tracker)
             tracker[s2[i]] -= 1
-            
-            if len(tracker) == 2:
-                return True
-            else: 
-                return False 
+            if tracker[i] == 0:
+                del tracker[i]
+            print(len(tracker))
+        if len(tracker) == 2:
+            return False
+        else: 
+            return True
 
 class TestStrings(unittest.TestCase):
     def test_isUnique(self):
@@ -49,9 +51,9 @@ class TestStrings(unittest.TestCase):
         s3 = "pales"
         s4 = "bale"
         s5 = "bake"
-        #self.assertTrue(one_away(s1, s2))
-        #self.assertTrue(one_away(s3, s1))
-        #self.assertTrue(one_away(s1, s4))
+        self.assertTrue(one_away(s1, s2))
+        self.assertTrue(one_away(s3, s1))
+        self.assertTrue(one_away(s1, s4))
         self.assertFalse(one_away(s1, s5))
 
 unittest.main()
